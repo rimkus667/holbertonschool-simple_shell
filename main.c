e0c7e860ba5f45172f4dbbadb6cc0dd04070cca7
root <karimhabouchi310@gmail.com>
2025-04-24 13:36:28 +0000

    task 2 test

---
+++ main.c
@@ -0,0 +1,52 @@
+#include "main.h"
+/**
+ * main - Minimal shell for Task 0.1
+ * Return: 0 on success
+ */
+int main(void)
+{
+    char *line = NULL;
+    size_t len = 0;
+    ssize_t read;
+    pid_t pid;
+    char *argv[2];
+    int interactive = isatty(STDIN_FILENO);
+
+    while (1)
+    {
+        if (interactive)
+            printf("#cisfun$ ");
+
+        read = getline(&line, &len, stdin);
+        if (read == -1)
+        {
+            free(line);
+            exit(0);
+        }
+
+        line[strcspn(line, "\n")] = '\0';
+
+        pid = fork();
+        if (pid == 0)
+        {
+            argv[0] = line;
+            argv[1] = NULL;
+
+            execve(argv[0], argv, environ);
+            perror("./shell");
+            exit(1);
+        }
+        else if (pid > 0)
+        {
+            wait(NULL);
+        }
+        else
+        {
+            perror("fork");
+        }
+    }
+
+    free(line);
+    return (0);
+}
+
---
+++ main.h
@@ -0,0 +1,13 @@
+#ifndef MAIN_H
+#define MAIN_H
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <unistd.h>
+#include <sys/wait.h>
+
+extern char **environ;
+
+
+#endif
